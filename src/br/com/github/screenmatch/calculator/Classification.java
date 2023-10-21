dc6a6b2fb1199949f3ed7090fa3479f90b6ed163
Guilherme <guilherme.sajs@gmail.com>
2023-10-21 04:02:43 -0300

    Adiciona interface 'Classification'

---
+++ src/br/com/github/screenmatch/calculator/Classification.java
@@ -0,0 +1,6 @@
+package br.com.github.screenmatch.calculator;
+
+public interface Classification {
+    int getClassification();
+
+}
--- src/br/com/github/screenmatch/models/Film.java
+++ src/br/com/github/screenmatch/models/Film.java
@@ -1,6 +1,8 @@
 package br.com.github.screenmatch.models;
 
-public class Film extends Title {
+import br.com.github.screenmatch.calculator.Classification;
+
+public class Film extends Title implements Classification {
   private String director;
 
     public String getDirector() {
@@ -9,5 +11,10 @@ public class Film extends Title {
 
     public void setDirector(String director) {
         this.director = director;
+    }
+
+    @Override
+    public int getClassification() {
+        return 0;
     }
 }
