4e9e0fd215f59a712af9c0672811454772c39b5e
Guilherme <guilherme.sajs@gmail.com>
2023-11-18 20:14:51 -0300

    Adiciona HttpResponse

--- src/br/com/github/screenmatch/main/MainSearch.java
+++ src/br/com/github/screenmatch/main/MainSearch.java
@@ -1,14 +1,19 @@
 package br.com.github.screenmatch.main;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class MainSearch {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("http://www.omdbapi.com/?t=The+Avengers&apikey=38aa991e"))
                 .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
     }
 }
