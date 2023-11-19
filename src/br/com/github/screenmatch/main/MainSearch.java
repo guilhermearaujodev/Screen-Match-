1ae9e9768d04761ce4b18c31ad29671efd577340
Guilherme <guilherme.sajs@gmail.com>
2023-11-19 04:23:14 -0300

    Adiciona scanner Com o scanner vamos conseguir selecionar qual é o filme que a API irá buscar.

--- src/br/com/github/screenmatch/main/MainSearch.java
+++ src/br/com/github/screenmatch/main/MainSearch.java
@@ -5,12 +5,18 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class MainSearch {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner reading = new Scanner (System.in);
+        System.out.println("Type a movie to search: ");
+        var search = reading.nextLine();
+        String address = "http://www.omdbapi.com/?t=" + search + "&apikey=38aa991e";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://www.omdbapi.com/?t=The+Avengers&apikey=38aa991e"))
+                .uri(URI.create(address))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
